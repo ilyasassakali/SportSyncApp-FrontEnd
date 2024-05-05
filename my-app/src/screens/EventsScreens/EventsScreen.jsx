7dcd11a331d22f2784894d3b2bbd6c6a3466ebc5
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2024-05-05 18:42:40 +0200

    Added TopTabNavigator to EventsScreen

--- my-app/src/screens/EventsScreens/EventsScreen.jsx
+++ my-app/src/screens/EventsScreens/EventsScreen.jsx
@@ -1,6 +1,174 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createMaterialTopTabNavigator();
+
+const EventCard = ({ event, onPress }) => (
+  <TouchableOpacity style={styles.cardContainer} onPress={onPress}>
+    <View style={styles.dateContainer}>
+      <Text style={styles.dateDay}>{event.day}</Text>
+      <Text style={styles.dateMonth}>{event.month}</Text>
+    </View>
+    <View style={styles.detailContainer}>
+      <Text style={styles.eventTitle} numberOfLines={1} ellipsizeMode="tail">{event.title}</Text>
+      <View style={styles.eventDetailItem}>
+        <Text style={styles.eventDetailsText}>{event.time}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+
+function UpNextEventsScreen({navigation}) {
+  return (
+    <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+        {events.map(event => (
+          <EventCard key={event.id} event={event} onPress={() => navigation.navigate('EventOverview', { event })}/>
+        ))}
+    </ScrollView>
+  );
+}
+
+function PastEventsScreen({navigation}) {
+  return (
+    <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+        {events.map(event => (
+          <EventCard key={event.id} event={event} onPress={() => navigation.navigate('EventOverview', { event })}/>
+        ))}
+    </ScrollView>
+  );
+}
+
+const EventsScreen = () => {
+  const [fabColor, setFabColor] = useState("#4CAF50");
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Events</Text>
+
+
+      <NavigationContainer independent={true}>
+        <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: 'white' },
+          tabBarIndicatorStyle: { backgroundColor: '#4CAF50' },
+          tabBarActiveTintColor: '#4CAF50',
+          tabBarInactiveTintColor: 'gray',
+        }}
+        
+        >
+          <Tab.Screen name="Up Next" component={UpNextEventsScreen} />
+          <Tab.Screen name="Past" component={PastEventsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+
+
+      <TouchableOpacity
+        style={[styles.fab, { backgroundColor: fabColor }]}
+        onPressIn={() => setFabColor("#449D48")}
+        onPressOut={() => setFabColor("#4CAF50")}
+        onPress={() => navigation.navigate('EventName')}
+        activeOpacity={1}
+      >
+        <Ionicons name="create-outline" size={30} color="#FFFFFF" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    justifyContent: 'flex-start',
+    paddingTop: 50
+  },
+  scrollView: {
+    width: '100%',
+    backgroundColor: "#FFFFFF",
+
+  },
+  header: {
+    paddingLeft: 20,
+    fontFamily: 'Poppins_Bold',
+    fontSize: 28,
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    padding: 10,  
+    borderBottomWidth: 0.5, 
+    borderBottomColor: '#ccc',   
+  },
+  dateContainer: {
+    marginRight: 30,
+    marginLeft:15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateDay: {
+    fontFamily: 'Poppins_SemiBold',
+    fontSize: 22,
+    color: "#333",
+    marginBottom: -11,
+    color: "#4CAF50"
+  },
+  dateMonth: {
+    fontFamily: 'Poppins_Regular',
+    fontSize: 18,
+    color: "#4CAF50",
+    marginBottom: 0,
+  },
+  detailContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  eventTitle: {
+    fontFamily: 'Poppins_SemiBold',
+    fontSize: 18,
+    color: "#333",
+    marginBottom: -1,
+  },
+  eventDetails: {
+    fontFamily: 'Poppins_Regular',
+    fontSize: 14,
+    color: "#666",
+  },
+  eventDetailItem: {
+    flexDirection: 'row', 
+    alignItems: 'center',  
+    
+  },
+  eventDetailsText: {
+    fontFamily: 'Poppins_Regular',
+    fontSize: 14,
+    color: "#4CAF50",
+    color:"#333"
+  },
+  fab: {
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  }
+});
+
+export default EventsScreen;
+
 
 // Simulated event data
 const events = [
@@ -74,134 +242,44 @@ const events = [
     hoste: "Ilyas Assakali",
     date: "Wednesday 16 Decembre"
   },
+  {
+    id: 8,
+    day: "30",
+    month: "Feb",
+    title: "Indoor Soccer 5v5",
+    time: "18:00 - 19:30",
+    location: "Leuzesteenweg 241, 9600 Ronse",
+    hoste: "Ilyas Assakali",
+    date: "Wednesday 16 Decembre"
+  },
+  {
+    id: 9,
+    day: "30",
+    month: "Feb",
+    title: "Indoor Soccer 5v5",
+    time: "18:00 - 19:30",
+    location: "Leuzesteenweg 241, 9600 Ronse",
+    hoste: "Ilyas Assakali",
+    date: "Wednesday 16 Decembre"
+  },
+  {
+    id: 10,
+    day: "30",
+    month: "Feb",
+    title: "Indoor Soccer 5v5",
+    time: "18:00 - 19:30",
+    location: "Leuzesteenweg 241, 9600 Ronse",
+    hoste: "Ilyas Assakali",
+    date: "Wednesday 16 Decembre"
+  },
+  {
+    id: 11,
+    day: "30",
+    month: "Feb",
+    title: "Indoor Soccer 5v5",
+    time: "18:00 - 19:30",
+    location: "Leuzesteenweg 241, 9600 Ronse",
+    hoste: "Ilyas Assakali",
+    date: "Wednesday 16 Decembre"
+  },
 ];
-
-const EventCard = ({ event, onPress }) => (
-  <TouchableOpacity style={styles.cardContainer} onPress={onPress}>
-    <View style={styles.dateContainer}>
-      <Text style={styles.dateDay}>{event.day}</Text>
-      <Text style={styles.dateMonth}>{event.month}</Text>
-    </View>
-    <View style={styles.detailContainer}>
-      <Text style={styles.eventTitle} numberOfLines={1} ellipsizeMode="tail">{event.title}</Text>
-      <View style={styles.eventDetailItem}>
-        <Text style={styles.eventDetailsText}>{event.time}</Text>
-      </View>
-    </View>
-  </TouchableOpacity>
-);
-
-
-const EventsScreen = ({ navigation }) => {
-  const [fabColor, setFabColor] = useState("#4CAF50");
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Events</Text>
-      <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
-        {events.map(event => (
-          <EventCard key={event.id} event={event} onPress={() => navigation.navigate('EventOverview', { event })}/>
-        ))}
-      </ScrollView>
-      {/* Floating Action Button */}
-      <TouchableOpacity
-        style={[styles.fab, { backgroundColor: fabColor }]}
-        onPressIn={() => setFabColor("#449D48")}
-        onPressOut={() => setFabColor("#4CAF50")}
-        onPress={() => navigation.navigate('EventName')}
-        activeOpacity={1}
-      >
-        <Ionicons name="create-outline" size={30} color="#FFFFFF" />
-      </TouchableOpacity>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    padding: 20,
-    paddingTop: 50
-  },
-  scrollView: {
-    width: '100%',
-  },
-  header: {
-    fontFamily: 'Poppins_Bold',
-    fontSize: 28,
-    marginBottom: 10,
-  },
-  cardContainer: {
-    flexDirection: 'row',
-    padding: 10,  
-    borderBottomWidth: 0.5, 
-    borderBottomColor: '#ccc',   
-  },
-  dateContainer: {
-    marginRight: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dateDay: {
-    fontFamily: 'Poppins_SemiBold',
-    fontSize: 22,
-    color: "#333",
-    marginBottom: -11,
-    color: "#4CAF50"
-  },
-  dateMonth: {
-    fontFamily: 'Poppins_Regular',
-    fontSize: 18,
-    color: "#4CAF50",
-    marginBottom: 0,
-  },
-  detailContainer: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  eventTitle: {
-    fontFamily: 'Poppins_SemiBold',
-    fontSize: 18,
-    color: "#333",
-    marginBottom: -1,
-  },
-  eventDetails: {
-    fontFamily: 'Poppins_Regular',
-    fontSize: 14,
-    color: "#666",
-  },
-  eventDetailItem: {
-    flexDirection: 'row', 
-    alignItems: 'center',  
-    
-  },
-  eventDetailsText: {
-    fontFamily: 'Poppins_Regular',
-    fontSize: 14,
-    color: "#4CAF50",
-    color:"#333"
-  },
-  fab: {
-    position: 'absolute',
-    right: 20,
-    bottom: 20,
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5
-  }
-});
-
-export default EventsScreen;
