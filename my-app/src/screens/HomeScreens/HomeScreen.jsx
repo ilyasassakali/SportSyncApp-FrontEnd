0f9cac19a998a6139bb27b67d3c06db34d0e391f
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2024-05-21 17:07:43 +0200

    Added NextUp event to HomeScreen

--- my-app/src/screens/HomeScreens/HomeScreen.jsx
+++ my-app/src/screens/HomeScreens/HomeScreen.jsx
@@ -1,34 +1,89 @@
 import React,{useState, useEffect} from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import * as SecureStore from 'expo-secure-store';
 import { useAuth } from "../../components/AuthContext";
+import { useEvents } from '../../components/EventsContext'; 
 
 
 const HomeScreen = ({ navigation }) => {
   const { userData } = useAuth();
+  const { events } = useEvents();
   const [buttonColor, setButtonColor] = useState("#4CAF50");
   const [fabColor, setFabColor] = useState("#4CAF50");
+  const [upcomingEvent, setUpcomingEvent] = useState(null);
+
+  useEffect(() => {
+    const now = new Date();
+    const upcomingEvents = events.filter(event => {
+      const [eventStartHour, eventStartMinute] = event.time.split(' - ')[0].split(':');
+      const eventDate = new Date(event.date);
+      eventDate.setHours(eventStartHour);
+      eventDate.setMinutes(eventStartMinute);
+      return eventDate >= now;
+    });
+    upcomingEvents.sort((a, b) => {
+      const eventADate = new Date(a.date);
+      const eventBDate = new Date(b.date);
+      const [eventAStartHour, eventAStartMinute] = a.time.split(' - ')[0].split(':');
+      const [eventBStartHour, eventBStartMinute] = b.time.split(' - ')[0].split(':');
+      eventADate.setHours(eventAStartHour);
+      eventADate.setMinutes(eventAStartMinute);
+      eventBDate.setHours(eventBStartHour);
+      eventBDate.setMinutes(eventBStartMinute);
+      return eventADate - eventBDate;
+    });
+    if (upcomingEvents.length > 0) {
+      setUpcomingEvent(upcomingEvents[0]);
+    } else {
+      setUpcomingEvent(null);
+    }
+  }, [events]);
+  
+  
 
   return(
   <View style={styles.container}>
     <Text style={styles.header}>Hello {userData ? userData.firstName : ""}!</Text>
     <Text style={styles.subTitle}>Up next</Text>
+
+
+
     <View style={styles.textContainer}>  
-      <Text style={styles.subText} >
-        Your upcoming sport events{"\n"}
-        will appear here...
-      </Text>
-      <TouchableOpacity
-          style={[styles.button, { backgroundColor: buttonColor }]}
-          onPressIn={() => setButtonColor("#449D48")} 
-          onPressOut={() => setButtonColor("#4CAF50")} 
-          onPress={() => navigation.navigate('EventName')}
-          activeOpacity={1}
-        >
-          <Text style={styles.buttonText}>Plan a Sport Event</Text>
-        </TouchableOpacity>
+    {upcomingEvent ? (
+          <TouchableOpacity style={styles.cardContainer} onPress={() => navigation.navigate('EventOverview', { event: upcomingEvent })}>
+            <View style={styles.dateContainer}>
+              <Text style={styles.dateDay}>{new Date(upcomingEvent.date).getDate()}</Text>
+              <Text style={styles.dateMonth}>{new Date(upcomingEvent.date).toLocaleString('en-US', { month: 'short' })}</Text>
+            </View>
+            <View style={styles.detailContainer}>
+              <Text style={styles.eventTitle} numberOfLines={1} ellipsizeMode="tail">{upcomingEvent.title}</Text>
+              <View style={styles.eventDetailItem}>
+                <Text style={styles.eventDetailsText}>{upcomingEvent.time}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <View>
+            <Text style={styles.subText} >
+              Your upcoming sport events{"\n"}
+              will appear here...
+            </Text>
+            <TouchableOpacity
+            style={[styles.button, { backgroundColor: buttonColor }]}
+            onPressIn={() => setButtonColor("#449D48")} 
+            onPressOut={() => setButtonColor("#4CAF50")} 
+            onPress={() => navigation.navigate('EventName')}
+            activeOpacity={1}
+          >
+            <Text style={styles.buttonText}>Plan a Sport Event</Text>
+          </TouchableOpacity>
+        </View>
+
+        )}
+      
     </View>
+
+
 
     <TouchableOpacity
         style={[styles.fab, { backgroundColor: fabColor }]}
@@ -103,7 +158,56 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5
-  }
+  },
+  cardContainer: {
+    flexDirection: 'row',
+    padding: 10,  
+  },
+  dateContainer: {
+    marginRight: 30,
+    marginLeft:15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateDay: {
+    fontFamily: 'Poppins_SemiBold',
+    fontSize: 22,
+    color: "#333",
+    marginBottom: -11,
+    color: "#4CAF50"
+  },
+  dateMonth: {
+    fontFamily: 'Poppins_Regular',
+    fontSize: 18,
+    color: "#4CAF50",
+    marginBottom: 0,
+  },
+  detailContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  eventTitle: {
+    fontFamily: 'Poppins_SemiBold',
+    fontSize: 18,
+    color: "#333",
+    marginBottom: -1,
+  },
+  eventDetails: {
+    fontFamily: 'Poppins_Regular',
+    fontSize: 14,
+    color: "#666",
+  },
+  eventDetailItem: {
+    flexDirection: 'row', 
+    alignItems: 'center',  
+    
+  },
+  eventDetailsText: {
+    fontFamily: 'Poppins_Regular',
+    fontSize: 14,
+    color: "#4CAF50",
+    color:"#333"
+  },
 });
 
 export default HomeScreen;
