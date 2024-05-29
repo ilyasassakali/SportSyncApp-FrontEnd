ca5c734de52193801d7d38229575c5e1a4e963ea
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2024-05-29 18:20:13 +0200

    Added basic push notification logic

--- my-app/src/screens/HomeScreens/HomeScreen.jsx
+++ my-app/src/screens/HomeScreens/HomeScreen.jsx
@@ -1,10 +1,21 @@
 import React,{useState, useEffect} from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Button, Platform } from "react-native";
 import { useAuth } from "../../components/AuthContext";
 import { useEvents } from '../../components/EventsContext'; 
 import FabButton from '../../components/FabButton';
 import * as SecureStore from 'expo-secure-store';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 const CountdownTimer = ({ targetDate }) => {
   const [timeRemaining, setTimeRemaining] = useState(getTimeRemaining(targetDate));
@@ -75,6 +86,92 @@ const HomeScreen = ({ navigation }) => {
   const { events, setEvents } = useEvents();
   const [buttonColor, setButtonColor] = useState("#4CAF50");
   const [upcomingEvent, setUpcomingEvent] = useState(null);
+
+  /*push notifications logic*/ 
+  const [expoPushToken, setExpoPushToken] = useState('');
+
+  useEffect(() => {
+    console.log("Registering for push notifications");
+    registerForPushNotificationsAsync().then(
+      token => { console.log("token: ", token);
+      setExpoPushToken(token)
+      })
+      .catch((err) => console.log(err))
+  }, []);
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+  
+    if (Platform.OS === 'android') {
+      await Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      // Learn more about projectId:
+      // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+      // EAS projectId is used here.
+      try {
+        const projectId =
+          Constants?.expoConfig?.extra?.eas?.projectId ??
+          Constants?.easConfig?.projectId;
+        if (!projectId) {
+          throw new Error('Project ID not found');
+        }
+        token = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId: "19e375a0-6e89-46ce-8fdb-485e73631b17",
+          })
+        ).data;
+        console.log(token);
+      } catch (e) {
+        token = `${e}`;
+      }
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    return token;
+  }
+
+
+  const sendNotification = async () => {
+    console.log("sending push notification...");
+    //notification message
+    const message = {
+      to: expoPushToken,
+      sound: "default",
+      title: "My first push notification",
+      body: "This is my first push notification made with expo"
+    }
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      header:{
+        host: "exp.host",
+        accept: "application/json",
+        "accept-encoding": "gzip, deflate",
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(message)
+    })
+  };
+
+  /*push notifications logic*/
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -160,6 +257,8 @@ const HomeScreen = ({ navigation }) => {
         </View>
         )}   
     </View>
+    <Button title="Send push notification" onPress={sendNotification}/>
+
     <FabButton/>
   </View>
   );
